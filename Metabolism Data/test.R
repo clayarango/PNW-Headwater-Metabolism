d177c0f1df8ab2b04d52dbcf12ac87a0fb8766d0
AJ Reisinger <reisingera@ufl.edu>
2019-12-04 14:15:14 -0500

    testing

--- Metabolism Data/test.R
+++ Metabolism Data/test.R
@@ -2,8 +2,22 @@
 ### Created by AJR
 ### Created on 2019-12-04
 
-PROJHOME
 
-site_path=R.home(component="home")
-fname=file.path(site_path, "etc", "Rprofile.site")
-file.exists(fname)
+### Read in data from git repo
+test.data<-read.csv(file="./Metabolism Data/test.csv")
+
+# LOAD O2 SATURATION FUNCTION
+
+osat<- function(temp, bp) {
+  tstd<-log((298.15-temp) / (273.15 + temp))
+  a0<-2.00907
+  a1<-3.22014
+  a2<-4.0501
+  a3<-4.94457
+  a4<- -0.256847
+  a5<- 3.88767
+  u<-10^(8.10765-(1750.286/(235+temp)))
+  sato<-(exp(a0 + a1*tstd + a2*tstd^2 + a3*tstd^3 + a4*tstd^4+a5*tstd^5))*((bp-u)/(760-u))*1.42905
+  sato
+}
+
