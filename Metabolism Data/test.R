ed29303c9e7aa9080b0cdbd8e42f5120b3cf4dcd
AJ Reisinger <reisingera@ufl.edu>
2019-12-04 11:46:40 -0500

    test

---
+++ Metabolism Data/test.R
@@ -0,0 +1,9 @@
+### Master script to analyze PNW Metabolism data via streammetabolizer
+### Created by AJR
+### Created on 2019-12-04
+
+PROJHOME
+
+site_path=R.home(component="home")
+fname=file.path(site_path, "etc", "Rprofile.site")
+file.exists(fname)
